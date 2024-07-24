a145c22f099150ee86ad423a02bfb444dcc34052
shecannotsee <shecannotsee@163.com>
2024-07-24 21:03:59 +0800

    [feat] 1.Add function note 2.Use templates to constrain T

--- src/timed_cache_queue.h
+++ src/timed_cache_queue.h
@@ -7,16 +7,47 @@
 
 namespace sheer {
 
+// Forward declaration of timed_cache_queue
+template <typename T>
+class timed_cache_queue;
+
+// Primary template for checking timestamp member
+template <typename T, typename = void>
+struct has_timestamp_member : std::false_type {};
+
+// Specialization for types that have a timestamp member
+template <typename T>
+struct has_timestamp_member<T, std::void_t<decltype(std::declval<T>().timestamp)>> : std::true_type {};
+
+/**
+ * @brief A thread-safe queue with timed element eviction based on their timestamps.
+ *
+ * @tparam T The type of elements stored in the queue. Must have a public member 'timestamp' of type
+ * std::chrono::time_point.
+ */
 template <typename T>
 class timed_cache_queue {
+  static_assert(has_timestamp_member<T>::value, "T must have a 'timestamp' member of type std::chrono::time_point");
+
  public:
+  /**
+   * @brief Deleted default constructor. Use explicit constructor to initialize.
+   */
   timed_cache_queue() = delete;
 
+  /**
+   * @brief Constructs a timed cache queue with a specified cache expiration time.
+   *
+   * @param cache_time_in_seconds The time in seconds after which elements are evicted from the queue.
+   */
   explicit timed_cache_queue(const int cache_time_in_seconds)
       : cache_time_(cache_time_in_seconds), stop_thread_(false) {
     cleanup_thread_ = std::thread(&timed_cache_queue::cleanup_expired_elements, this);
   }
 
+  /**
+   * @brief Destructor. Stops the cleanup thread and waits for it to join.
+   */
   ~timed_cache_queue() {
     stop_thread_ = true;
     cv_.notify_all();
@@ -26,14 +57,17 @@ class timed_cache_queue {
   }
 
  private:
-  std::queue<T> queue_;
-  int cache_time_;
-  mutable std::mutex mutex_;
-  std::condition_variable cv_;
-  std::thread cleanup_thread_;
-  bool stop_thread_;
+  std::queue<T> queue_;         ///< The queue storing elements of type T.
+  int cache_time_;              ///< Time in seconds after which elements are considered expired.
+  mutable std::mutex mutex_;    ///< Mutex to synchronize access to the queue.
+  std::condition_variable cv_;  ///< Condition variable to signal changes in the queue.
+  std::thread cleanup_thread_;  ///< Thread for periodically cleaning up expired elements.
+  bool stop_thread_;            ///< Flag to signal the cleanup thread to stop.
 
  private:
+  /**
+   * @brief Worker function for the cleanup thread that removes expired elements from the queue.
+   */
   void cleanup_expired_elements() {
     while (!stop_thread_) {
       std::unique_lock<std::mutex> lock(mutex_);
@@ -51,16 +85,33 @@ class timed_cache_queue {
   }
 
  public:
+  /**
+   * @brief Returns the number of elements currently in the queue.
+   *
+   * @return size_t The number of elements in the queue.
+   */
   size_t size() const {
     std::lock_guard<std::mutex> lock(mutex_);
     return queue_.size();
   }
 
+  /**
+   * @brief Adds an element to the queue.
+   *
+   * @param element The element to be added to the queue.
+   */
   void push(const T& element) {
     std::lock_guard<std::mutex> lock(mutex_);
     queue_.push(element);
   }
 
+  /**
+   * @brief Removes the front element from the queue, if available.
+   *
+   * @param element Reference to store the popped element.
+   * @return true If an element was successfully popped.
+   * @return false If the queue was empty.
+   */
   bool pop(T& element) {
     std::lock_guard<std::mutex> lock(mutex_);
     if (queue_.empty()) {
