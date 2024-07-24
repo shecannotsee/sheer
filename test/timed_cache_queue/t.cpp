d1a4f81e77bad0099404ec3747f351139866e096
shecannotsee <shecannotsee@163.com>
2024-07-24 20:41:02 +0800

    [feat] 1.Add namespace test

--- test/timed_cache_queue/t.cpp
+++ test/timed_cache_queue/t.cpp
@@ -1,20 +1,22 @@
 #include <she_test.h>
 
-#include "timed_cache_queue.h"
+#include "sheer.h"
 
 namespace {
 struct test_struct {
   std::chrono::time_point<std::chrono::system_clock> timestamp;  // 时间戳
-  test_struct(std::chrono::time_point<std::chrono::system_clock> ts) : timestamp(ts) {}
+  test_struct(std::chrono::time_point<std::chrono::system_clock> ts) : timestamp(ts) {
+  }
 };
+
 }  // namespace
 
 SHE_TEST(timed_cache_queue_test, main) {
-  timed_cache_queue<test_struct> cache(10);
+  sheer::timed_cache_queue<test_struct> cache(10);
 
   // 模拟添加几个元素
   cache.push(test_struct(std::chrono::system_clock::now()));  // 当前时间戳
-  std::this_thread::sleep_for(std::chrono::seconds(5));        // 等待5秒
+  std::this_thread::sleep_for(std::chrono::seconds(5));       // 等待5秒
   cache.push(test_struct(std::chrono::system_clock::now()));  // 当前时间戳
 
   // 测试获取元素
