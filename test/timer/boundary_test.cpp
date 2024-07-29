5680261829d472bc65fe6afeca9be8c731f5a6a3
shecannotsee <shecannotsee@163.com>
2024-07-29 10:52:28 +0800

    [test] 1.Add times test:sample and boundary test

---
+++ test/timer/boundary_test.cpp
@@ -0,0 +1,56 @@
+#include <she_test.h>
+#include <timer.h>
+
+#include <thread>
+
+SHE_TEST(test_timer, boundary_test) {
+  {
+    sheer::timer record;
+    record.end();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    record.start();
+    std::cout << "     seconds: " << record.get_elapsed<std::chrono::seconds>()      << " passed." << std::endl;
+    std::cout << "milliseconds: " << record.get_elapsed<std::chrono::milliseconds>() << " passed." << std::endl;
+    std::cout << "microseconds: " << record.get_elapsed<std::chrono::microseconds>() << " passed." << std::endl;
+  }
+
+  {
+    sheer::timer record_without_init;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    try {
+      std::cout << "     seconds: " << record_without_init.get_elapsed<std::chrono::seconds>()      << " passed.\n";
+      std::cout << "milliseconds: " << record_without_init.get_elapsed<std::chrono::milliseconds>() << " passed.\n";
+      std::cout << "microseconds: " << record_without_init.get_elapsed<std::chrono::microseconds>() << " passed.\n";
+    } catch (const std::runtime_error& ex) {
+      std::cout << ex.what() << std::endl;
+    }
+  }
+
+  {
+    sheer::timer record_without_start;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    record_without_start.end();
+    try {
+      std::cout << "     seconds: " << record_without_start.get_elapsed<std::chrono::seconds>()      << " passed.\n";
+      std::cout << "milliseconds: " << record_without_start.get_elapsed<std::chrono::milliseconds>() << " passed.\n";
+      std::cout << "microseconds: " << record_without_start.get_elapsed<std::chrono::microseconds>() << " passed.\n";
+    } catch (const std::runtime_error& ex) {
+      std::cout << ex.what() << std::endl;
+    }
+  }
+
+  {
+    sheer::timer record_without_end;
+    record_without_end.start();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    try {
+      std::cout << "     seconds: " << record_without_end.get_elapsed<std::chrono::seconds>()      << " passed.\n";
+      std::cout << "milliseconds: " << record_without_end.get_elapsed<std::chrono::milliseconds>() << " passed.\n";
+      std::cout << "microseconds: " << record_without_end.get_elapsed<std::chrono::microseconds>() << " passed.\n";
+    } catch (const std::runtime_error& ex) {
+      std::cout << ex.what() << std::endl;
+    }
+  }
+
+  return true;
+}
---
+++ test/timer/sample.cpp
@@ -0,0 +1,18 @@
+#include <she_test.h>
+#include <timer.h>
+
+#include <thread>
+
+SHE_TEST(test_timer, sample) {
+  sheer::timer record;
+
+  record.start();
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  record.end();
+
+  std::cout << "     seconds: " << record.get_elapsed<std::chrono::seconds>() << " passed." << std::endl;
+  std::cout << "milliseconds: " << record.get_elapsed<std::chrono::milliseconds>() << " passed." << std::endl;
+  std::cout << "microseconds: " << record.get_elapsed<std::chrono::microseconds>() << " passed." << std::endl;
+
+  return true;
+}
