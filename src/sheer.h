792c8de3f1aa3874c5b6002f4dbd63e0dc6a0cc1
shecannotsee <shecannotsee@163.com>
2024-07-29 11:34:15 +0800

    [feat] 1.Add custom namespace

--- src/sheer.h
+++ src/sheer.h
@@ -1,5 +1,12 @@
-#include "timed_cache_queue.h"
+#ifndef SHEER_H
+#define SHEER_H
 
-namespace sheer {
+#include "timed_cache_queue.h"
+#include "timer.h"
+
+namespace test_support {
 using sheer::timed_cache_queue;
+using sheer::timer;
 }
+
+#endif  // SHEER_H
