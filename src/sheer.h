1dabda4e02094d9b5564a01ba58e1b93fcbabb1a
shecannotsee <shecannotsee@163.com>
2024-07-29 11:52:23 +0800

    [feat] 1.using namespace test_support to wrapper

--- src/sheer.h
+++ src/sheer.h
@@ -1,12 +1,16 @@
 #ifndef SHEER_H
 #define SHEER_H
 
+#include "file_system.h"
+#include "io.h"
 #include "timed_cache_queue.h"
 #include "timer.h"
 
 namespace test_support {
+using sheer::file_system;
+using sheer::io;
 using sheer::timed_cache_queue;
 using sheer::timer;
-}
+}  // namespace test_support
 
 #endif  // SHEER_H
