a32fe78d9bec72d2449c2bf8ba8588053381a466
shecannotsee <shecannotsee@163.com>
2024-07-29 15:01:22 +0800

    [test] 1.Add the test for date_time.h

---
+++ test/date_time/sample.cpp
@@ -0,0 +1,18 @@
+#include <she_test.h>
+#include <sheer.h>
+
+#include <thread>
+
+SHE_TEST(test_date_time, sample) {
+  using record = test_support::date_time;
+
+  const auto now = record::get_time_point();
+
+  std::cout << "--------date: " << record::format_date_string(now) << std::endl;
+  std::cout << "date with ms: " << record::format_date_string_with_milliseconds(now) << std::endl;
+  std::cout << "---timestamp: " << record::to_duration_number<std::chrono::seconds>(now) << std::endl;
+  std::cout << "----------ms: " << record::to_duration_number<std::chrono::milliseconds>(now) << std::endl;
+  std::cout << "----------us: " << record::to_duration_number<std::chrono::microseconds>(now) << std::endl;
+
+  return true;
+}
